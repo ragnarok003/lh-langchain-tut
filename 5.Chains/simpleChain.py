de01df867d65e65d7e921268fa0242f4226487fc
ragnarok003 <hariragave@gmail.com>
2026-04-12 19:56:29 +0530

    simple chain

---
+++ 5.Chains/simpleChain.py
@@ -0,0 +1,18 @@
+from langchain_ollama import ChatOllama
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from rich.console import Console
+from rich.markdown import Markdown
+csl = Console()
+
+
+
+model = ChatOllama(model="gemma4", temperature=0.0)
+
+prompt =PromptTemplate(template="generate 3 facts about a topic {topic}",input_variables=['topic'])
+
+parser =StrOutputParser()
+chain =prompt | model | parser
+result=chain.invoke({"topic":"aliens"})
+
+csl.print(Markdown(result))
