92461a7b975b27966031a24578c1827b43a9893c
ragnarok003 <hariragave@gmail.com>
2026-04-19 07:46:22 +0530

    embedding

---
+++ 8.embeddings/1.embedding.py
@@ -0,0 +1,26 @@
+from langchain_ollama import OllamaEmbeddings
+from rich.console import Console
+import logging
+
+
+logging.basicConfig(
+    filename='8.embeddings/embedding.log',
+    level=logging.INFO,
+    encoding='utf-8',
+)
+
+csl =Console()
+
+embeddings1 =OllamaEmbeddings(model="mxbai-embed-large")
+embeddings2 =OllamaEmbeddings(model="embeddinggemma:latest")
+text = "New York is a busy city with a lot of population"
+
+result1 = embeddings1.embed_query(text)
+result2 = embeddings2.embed_query(text)
+
+logging.info("mxbai-embed-large")
+logging.info(result1)
+logging.info("embeddinggemma:latest")
+logging.info(result2)
+
+print(len(result1), len(result2))
