9f851a4db570488849581066423904d05fbdd19b
ragnarok003 <hariragave@gmail.com>
2026-05-02 18:32:25 +0530

    sql agent

---
+++ 11.sql/app.py
@@ -0,0 +1,24 @@
+from langchain_ollama import ChatOllama
+from langchain_community.utilities import SQLDatabase
+from langchain_community.agent_toolkits import create_sql_agent
+from rich.console import Console
+from rich.markdown import Markdown
+
+csl =Console()
+
+db =SQLDatabase.from_uri("sqlite:///11.sql/chinook.db")
+
+llm =ChatOllama(
+    model="gemma4:e2b",
+    temperature=0.0
+)
+
+agent_executor = create_sql_agent(llm=llm,db=db,agent_type="tool-calling",verbose=True)
+
+# result =agent_executor.invoke({"input":"List all customers with their full names and email addresses."})
+# result =agent_executor.invoke({"input":"List the top 5 customers who spent the most money."})
+result =agent_executor.invoke({"input":"List employees and the number of customers they support."})
+
+
+csl.print(Markdown(result['input']))
+csl.print(Markdown(result['output']))
---
+++ 9.vectordb/pgvector/1.pgvector.py
@@ -0,0 +1,75 @@
+from langchain_ollama import OllamaEmbeddings
+from langchain_postgres import PGVector
+from langchain_core.documents import Document
+
+from rich.console import Console
+
+csl = Console()
+embeddings = OllamaEmbeddings(model="mxbai-embed-large:latest")
+
+connection = (
+    "postgresql+psycopg://postgres:Pass%40123@localhost:5432/lh"  # Uses psycopg3!
+)
+collection_name = "my_docs"
+
+vector_store = PGVector(
+    embeddings=embeddings,
+    collection_name=collection_name,
+    connection=connection,
+    use_jsonb=True,
+)
+
+
+docs = [
+    Document(
+        page_content="there are cats in the pond",
+        metadata={"id": 1, "location": "pond", "topic": "animals"},
+    ),
+    Document(
+        page_content="ducks are also found in the pond",
+        metadata={"id": 2, "location": "pond", "topic": "animals"},
+    ),
+    Document(
+        page_content="fresh apples are available at the market",
+        metadata={"id": 3, "location": "market", "topic": "food"},
+    ),
+    Document(
+        page_content="the market also sells fresh oranges",
+        metadata={"id": 4, "location": "market", "topic": "food"},
+    ),
+    Document(
+        page_content="the new art exhibit is fascinating",
+        metadata={"id": 5, "location": "museum", "topic": "art"},
+    ),
+    Document(
+        page_content="a sculpture exhibit is also at the museum",
+        metadata={"id": 6, "location": "museum", "topic": "art"},
+    ),
+    Document(
+        page_content="a new coffee shop opened on Main Street",
+        metadata={"id": 7, "location": "Main Street", "topic": "food"},
+    ),
+    Document(
+        page_content="the book club meets at the library",
+        metadata={"id": 8, "location": "library", "topic": "reading"},
+    ),
+    Document(
+        page_content="the library hosts a weekly story time for kids",
+        metadata={"id": 9, "location": "library", "topic": "reading"},
+    ),
+    Document(
+        page_content="a cooking class for beginners is offered at the community center",
+        metadata={"id": 10, "location": "community center", "topic": "classes"},
+    ),
+]
+
+vector_store.add_documents(docs, ids=[doc.metadata["id"] for doc in docs])
+
+results = vector_store.similarity_search("market", k=3)
+for doc in results:
+    csl.print(f"* {doc.page_content} [{doc.metadata}]")
+
+
+retrievers = vector_store.as_retriever(search_type="mmr",search_kwargs={"k":3})
+
+csl.print(retrievers.invoke("market"))
